7d038f5f48f718945b3087299c60c109833d6555
HerosCampos <herosrac@gmail.com>
2021-10-05 23:47:01 -0300

    Início do projeto

---
+++ MMO/NetCommon/Header Files/net_common.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+#include <pthread.h>
+#include <chrono>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <algorithm>
+#include <cstdint>
