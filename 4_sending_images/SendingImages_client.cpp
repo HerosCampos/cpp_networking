04f9386e609f2b6024b88b193adea8e56b93f61a
HerosCampos <herosrac@gmail.com>
2021-10-07 00:31:42 -0300

    Enviando imagens com BOOST ASIO

---
+++ 4_sending_images/SendingImages_client.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+#include <pthread.h>
+#include <chrono>
+#include <fstream>
+
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+
+        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), 5000);
+
+        boost::asio::ip::tcp::socket socket(io_context);
+
+        socket.connect(endpoint);
+
+        std::array<char, 193191> buffer;
+        std::ifstream file("confia.jpeg", std::ifstream::binary);
+        file.read(buffer.data(), buffer.size());
+
+        socket.write_some(boost::asio::buffer(buffer));
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}
